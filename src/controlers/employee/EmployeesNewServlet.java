67c15764a35fa195baeea581155ff5c5881f5441
yuito.kumasaka <yuito.kumasaka@growship.com>
2021-04-12 10:40:02 +0900

    add employee.new

--- src/controlers/employee/EmployeesNewServlet.java
+++ src/controlers/employee/EmployeesNewServlet.java
@@ -2,11 +2,14 @@ package controlers.employee;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Employee;
 
 /**
  * Servlet implementation class EmployeesNewServlet
@@ -20,16 +23,17 @@ public class EmployeesNewServlet extends HttpServlet {
      */
     public EmployeesNewServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        response.getWriter().append("Served at: ").append(request.getContextPath());
-    }
+        request.setAttribute("_token", request.getSession().getId());
+        request.setAttribute("employee", new Employee());
 
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/employees/new.jsp");
+        rd.forward(request, response);
+    }
 
 }
